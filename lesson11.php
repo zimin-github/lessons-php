47f6ed6c1a1b4d4e977e6a0f4d820cf09f63c05e
Vladimir Zimin <zimin.info@gmail.com>
2017-07-20 17:15:36 +0400

    make switch operator

---
+++ lesson11.php
@@ -0,0 +1,18 @@
+<?php
+// Операто switch
+ $x = 13;
+
+ switch ($x) {
+ 		case 0: echo "x равен нулю";
+ 			break;
+ 		case 1: echo "x равен единице";
+ 			break;
+ 		case 2: echo "x равен двум";
+ 			break;
+ 		case 3: echo "x равен трем";
+ 			break;
+ 		default: echo " Ничего не подходит";
+ }
+
+
+?>
