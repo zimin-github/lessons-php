f676d25697fbaafc0fa149869e870678c35f4547
Vladimir Zimin <zimin.info@gmail.com>
2017-07-20 12:00:10 +0400

    make string operations

---
+++ lesson6.php
@@ -0,0 +1,15 @@
+<?php
+// Строковые операции
+$var = 15;
+$str_1 = "Переменная var = $var\n Новая строка";
+$str_2 = '<b>Переменная var = $var\n Новая строка</b>';
+echo $str_1. "<br>";
+echo $str_2. "<br>";
+
+$str_1 = "string1";
+$str_2 = "string2";
+
+echo $str_1." ".$str_2;
+echo "<br>";
+echo "\\";
+?>
