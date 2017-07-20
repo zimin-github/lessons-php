f5824f4cd3db6cecf57c9f65ed757fdcb25800f0
Vladimir Zimin <zimin.info@gmail.com>
2017-07-20 10:27:19 +0400

    make variable and them type

---
+++ lesson2.php
@@ -0,0 +1,12 @@
+<?php
+// Переменные и их типы (скалярные)
+	$integer = 5;
+	$float = -3.7;
+	$string = "String";
+	$boolean = true;
+
+	echo "Переменная integer = $integer <br>";
+	echo "Переменная float = $float <br>";
+	echo "Переменная string = $string <br>";
+	echo "Переменная boolean = $boolean <br>";
+?>
