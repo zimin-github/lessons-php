5137aafe7611880bb1e5fc7544e778bbeedaa232
Vladimir Zimin <zimin.info@gmail.com>
2017-07-20 11:15:39 +0400

    make CONSTANTS, use functions define and defined

---
+++ lesson4.php
@@ -0,0 +1,15 @@
+<?php
+//Константы
+	echo "PHP_VERSION =".PHP_VERSION;
+	echo "<br>";
+	echo "PHP_OS = ".PHP_OS;
+
+	define("PI",3.1415);
+	echo "<br>". PI;
+	echo "<br>";
+	echo defined("PI");
+	echo "<br>";
+	echo defined("abrakadabra");
+
+
+?>
